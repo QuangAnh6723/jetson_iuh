e7a8cf8afdbc1611a6b86b86017b7a1ca19a2e22
anh linux <lequanganh6723@gmail.com>
2024-09-27 07:45:44 +0700

    add cmd for uart

--- robot_arm/arm.py
+++ robot_arm/arm.py
@@ -1,3 +1,16 @@
+import time
+import serial
+
+ser = serial.Serial(
+    port='/dev/ttyTHS1',
+    #port='COM3',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
+
 __pos_init = 'init'
 __pos_0 = 'pos0'
 __pos_1 = 'pos1'
@@ -6,11 +19,31 @@ __pos_2 = 'pos2'
 def go_pos(pos: int):
     print('robot going to',end=' ')
     if pos == 0:
-        print(__pos_0)
+        print('pos 0 ')
+        # print(__pos_0)
+        ser.write(b'#9P1259#11P1259#13P1017#15P845#17P1155#19P1224T500D500\r\n')
+        time.sleep(0.2)
     elif pos == 1:
-        print(__pos_1)
+        print('pos 1 ')
+        # print(__pos_1)
+        ser.write(b'#9P1259#11P1259#13P1017#15P741#17P1155#19P1569T500D500r\n')
+        time.sleep(0.2)
     elif pos == 2:
-        print(__pos_2)
+        print('pos 2 ')
+        # print(__pos_2)
+        ser.write(b'#9P1259#11P1259#13P1017#15P741#17P1155#19P2121T500D500\r\n')
+        time.sleep(0.2)
     else:
-        print(__pos_init)
+        print('pos init ')
+        ser.write(b'#9P1259#11P1397#13P1500#15P845#17P1569#19P1500T500D500\r\n')
+        time.sleep(0.2)
+        ser.write(b'#9P1259#11P1397#13P1017#15P845#17P1569#19P1500T500D500\r\n')
+        time.sleep(0.2)
+        ser.write(b'#9P1259#11P1397#13P1017#15P845#17P1155#19P1500T500D500\r\n')
+        time.sleep(0.2)
+        ser.write(b'#9P1259#11P1259#13P1017#15P845#17P1155#19P845T500D500\r\n')
+        time.sleep(0.2)
+        
+        # print(__pos_init)
+
     
